b6346d2b3769a6cbf7b246c0a0d4fecc26d8ee14
Porter Child <pchild28@gmail.com>
2017-11-01 10:46:07 -0600

    got coordinates from zip, and zips in radius

--- zip_bordering.py
+++ zip_bordering.py
@@ -1,18 +1,41 @@
 import zipcode as zp
-entryzip = '84339'
-myzip = zp.isequal(entryzip)
-print "Zip code: " 
-print entryzip
-print myzip.state
-print myzip.city
-print myzip.to_dict()
+#entryzip = '84339'
+#myzip = zp.isequal(entryzip)
+#print "Zip code: " 
+#print entryzip
+#print myzip.state
+#print myzip.city
+#print myzip.to_dict()
 
-coor = (41.640085, -111.935210)
-print "within 10 miles:"
-print zp.isinradius(coor, 10)
-print "within 20 miles:"
-print zp.isinradius(coor, 20)
-print "within 50 miles:"
-print zp.isinradius(coor, 50)
-print "within 200 miles:"
-print zp.isinradius(coor, 200)
+#coor = (41.640085, -111.935210)
+#print "within 10 miles:"
+#print zp.isinradius(coor, 10)
+
+
+import csv
+data = csv.reader(open("Shootings-data-10-31-2017-.csv"))
+next(data, None) #skip labels
+for row in data:
+	date = row[0]
+	print date
+	event_zip = row[2]
+	if event_zip is None:
+		continue
+	database = csv.reader(open("US Zip Codes from 2013 Government Data"))
+	for entry in database:
+		if str(entry[0]) == str(event_zip):
+			coor = entry[1:3]
+			coor[0] = float(coor[0])
+			coor[1] = float(coor[1])
+			coor = tuple(coor)
+			print event_zip
+			print coor
+			tfmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,25)]
+			fifmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,50)]
+			hundmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,100)]
+			if len(hundmile) == 0:
+				string = input("stop")
+			print "25: \n" + str(tfmile)
+			print "fifty: \n" + str(fifmile)
+			print "hundred: \n" + str(hundmile)
+			break
