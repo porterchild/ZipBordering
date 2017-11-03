bee01bc48508698f9ad8ae4feb64d34bea42869a
Porter Child <pchild28@gmail.com>
2017-11-03 16:15:26 -0600

    done, added hardcoded missing zips that werent included with the zipcode library. Just need a few more event zipcodes and write to a file

--- zip_bordering.py
+++ zip_bordering.py
@@ -6,20 +6,26 @@ import zipcode as zp
 #print myzip.state
 #print myzip.city
 #print myzip.to_dict()
-
-#coor = (41.640085, -111.935210)
-#print "within 10 miles:"
 #print zp.isinradius(coor, 10)
 
+def pull_from_hardcoded_zips(zipcode, dist):
+	with open("missing_zips.csv", "r") as file:
+		zips = csv.reader(file)
+		for line in zips:
+			if str(line[0]) == str(zipcode) and str(line[1]) == str(dist):
+				#print line[2:]
+				return line[2:]
 
 import csv
 data = csv.reader(open("Shootings-data-10-31-2017-.csv"))
+output = csv.writer(open("shootings_radius.csv"))
 next(data, None) #skip labels
-for row in data:
+for row in data:#every shooting
+	flag = False
 	date = row[0]
-	print date
 	event_zip = row[2]
-	if event_zip is None:
+	if event_zip is '':
+		print "missing event_zip"
 		continue
 	database = csv.reader(open("US Zip Codes from 2013 Government Data"))
 	for entry in database:
@@ -28,14 +34,41 @@ for row in data:
 			coor[0] = float(coor[0])
 			coor[1] = float(coor[1])
 			coor = tuple(coor)
-			print event_zip
-			print coor
+			#print event_zip
+			#print coor
 			tfmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,25)]
 			fifmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,50)]
 			hundmile = [str(x).replace("<Zip: ", "").replace(">", "") for x in zp.isinradius(coor,100)]
 			if len(hundmile) == 0:
-				string = input("stop")
-			print "25: \n" + str(tfmile)
-			print "fifty: \n" + str(fifmile)
-			print "hundred: \n" + str(hundmile)
+				print event_zip
+				#string = raw_input("stop")
+				tfmile = pull_from_hardcoded_zips(event_zip, 25)
+				fifmile = pull_from_hardcoded_zips(event_zip, 50)
+				hundmile = pull_from_hardcoded_zips(event_zip, 100)
+				flag = True
+			#print "25: \n" + str(tfmile)
+			#print "fifty: \n" + str(fifmile)
+			#print "hundred: \n" + str(hundmile)
 			break
+	output_line = []
+	count = 0
+	for zipcode in tfmile:
+		print [date, zipcode, 1, 1, 1, event_zip] 
+		count += 1
+	for zipcode in fifmile:
+		if zipcode not in tfmile:
+			count += 1
+			print [date, zipcode, 0, 1, 1, event_zip] 
+	hundcount = 0
+	for zipcode in hundmile:
+		if zipcode not in tfmile and zipcode not in fifmile:
+			hundcount += 1
+			print [date, zipcode, 0, 0, 1, event_zip] 
+	if flag is True:
+		flag = False
+	#print "fifties and twenties"
+	#print count
+	#print "len(hhundreds)"
+	#print len(hundmile)
+	#print "hundreds"
+	#print hundcount
